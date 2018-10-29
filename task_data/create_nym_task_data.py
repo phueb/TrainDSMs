cb8d7850dd993bed829714454ab597984236cd1e
phueb <phueb001@ucr.edu>
2018-10-29 11:14:17 -0500

    add antonym scraping from thesaurus.com

--- task_data/create_nym_task_data.py
+++ task_data/create_nym_task_data.py
@@ -10,11 +10,11 @@ from src import config
 from src.embedders.base import EmbedderBase
 
 CORPUS_NAME = 'childes-20180319'
-NYM_TYPE = 'synonym'  # TODO test antonym
+NYM_TYPE = 'antonym'  # TODO test antonym
 POS = 'noun'
 LEMMATIZE = True
-NUM_SYNS = 5
-VERBOSE = False
+NUM_NYMS = 5
+VERBOSE = True
 
 
 EXCLUDED = {'verb': ['do', 'is', 'be', 'wow', 'was', 'did', 'are',
@@ -35,7 +35,14 @@ EXCLUDED = {'verb': ['do', 'is', 'be', 'wow', 'was', 'did', 'are',
                      'valentine', 'po', 'moira']}
 
 
-async def fetch(session, w, verbose=False):
+async def get_nyms(w):
+    with aiohttp.ClientSession() as session:
+        html = await fetch(session, w)
+    nyms = scrape_nyms(html, w)
+    return nyms
+
+
+async def fetch(session, w, verbose=VERBOSE):
     url = 'http://www.thesaurus.com/browse/' + w
     if verbose:
         print('Fetching from {}'.format(url))
@@ -45,39 +52,59 @@ async def fetch(session, w, verbose=False):
         return await response.text()
 
 
-def scrape_nyms(page, w, verbose=False):
+def scrape_nyms(page, w, verbose=VERBOSE):  # TODO remove antonym scraping? - collapse all POS for antonyms
     if verbose:
-        print('\nScraping nyms for "{}"'.format(w))
-    res = []
+        print('\nScraping {}s for "{}"'.format(NYM_TYPE, w))
     soup = BeautifulSoup(page, 'lxml')
-    for section in soup.find_all('section', {'class': '{}s-container'.format(NYM_TYPE)}):
-        try:
-            found_pos = section.select('em')[0].text.strip()  # need to strip
-            if verbose:
-                print('\tFound"{}" section'.format(found_pos))
-        except IndexError:
-            if verbose:
-                print('\tSkipping section. No POS info available.')
+    try:
+        section = soup.find_all('section', {'class': 'synonyms-container'})[0]  # TODO iterate over multiple sections
+    except IndexError:
+        return []
 
-            # TODO this happens in noun sections only
-            # TODO there must b better way to find verb section when noun section is found first
-
+    try:
+        found_pos = section.select('em')[0].text.strip()  # need to strip
+        if verbose:
+            print('\tFound "{}" section'.format(found_pos))
+    except IndexError:
+        if verbose:
+            print('\tSkipping. No POS info available.')  # TODO better way to find verb section when noun section is found first
+        return []
+    else:
+        if POS == found_pos:
+            if NYM_TYPE == 'synonym':
+                res = find_synonyms(section)
+            elif NYM_TYPE == 'antonym':
+                try:
+                    section = soup.find_all('section', {'class': 'antonyms-container'})[0]
+                except IndexError:  # no antonyms
+                    print('No antonyms found')
+                    res = []
+                else:
+                    res = find_antonyms(section)
+            else:
+                raise AttributeError('Invalid arg to "nym_type".')
+            #
+            if len(res) > NUM_NYMS:
+                res = res[:NUM_NYMS]
+            return res
         else:
-            if POS == found_pos:
-                for li in section.find_all('li'):
-                    if len(li.text.split()) == 1:
-                        res.append(li.text)
-                break
-    if len(res) > NUM_SYNS:
-        res = res[:NUM_SYNS]
+            return []
+
+
+def find_synonyms(section):
+    res = []
+    for li in section.find_all('li'):
+        if len(li.text.split()) == 1:
+            res.append(li.text)
     return res
 
 
-async def get_nyms(w):
-    with aiohttp.ClientSession() as session:
-        html = await fetch(session, w)
-    nyms = scrape_nyms(html, w)
-    return nyms
+def find_antonyms(section):
+    res = []
+    for a in section.select('a'):
+        antonym = a.text.strip()
+        res.append(antonym)
+    return res
 
 
 if __name__ == '__main__':
@@ -96,9 +123,7 @@ if __name__ == '__main__':
                     probes.append(w)
         if LEMMATIZE:
             probes = set([p for p in probes if p in vocab])  # lemmas may not be in vocab
-        task_name = '{}_{}_matching'.format(POS, NYM_TYPE)
-        out_fname = '{}_{}.txt'.format(CORPUS_NAME, vocab_size)
-        out_path = config.Dirs.tasks / task_name / out_fname
+        out_path = config.Dirs.tasks / '{}_{}s'.format(POS, NYM_TYPE) / '{}_{}.txt'.format(CORPUS_NAME, vocab_size)
         if not out_path.parent.exists():
             out_path.parent.mkdir()
         with out_path.open('w') as f:
