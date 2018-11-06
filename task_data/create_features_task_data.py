fbfdb19b487504c72e6d3bc89670405bf70b3d64
phueb <phueb001@ucr.edu>
2018-11-05 19:21:29 -0600

    draft of script extracting feature data from mcrae

---
+++ task_data/create_features_task_data.py
@@ -0,0 +1,72 @@
+import string
+from spacy.lemmatizer import Lemmatizer
+from spacy.lang.en import LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES
+import pandas as pd
+
+from src import config
+from src.embedders.base import EmbedderBase
+
+CORPUS_NAME = 'childes-20180319'
+VERBOSE = True
+LEMMATIZE = True
+
+
+if __name__ == '__main__':
+    lemmatizer = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
+    for vocab_size in config.Task.vocab_sizes:
+        # process features data
+        in_path = config.Dirs.tasks / 'semantic_features' / 'mcrae_features.csv'
+        df = pd.read_csv(in_path, index_col=False)
+        concepts = [w.split('_')[0] for w in df['Concept']]
+        print('Number of unique concept words={}'.format(len(df['Concept'].unique())))
+
+        # TODO features
+
+
+
+
+        # make probes
+        vocab = EmbedderBase.load_corpus_data(num_vocab=vocab_size)[1]
+        assert len(vocab) == vocab_size
+        probes = []
+        for w in vocab:
+            if len(w) > 1:
+                if w[0] not in list(string.punctuation) \
+                        and w[1] not in list(string.punctuation):
+                    if LEMMATIZE:
+                        for pos in ['noun', 'verb', 'adj']:
+                            w = lemmatizer(w, pos)[0]
+                            if w in concepts:
+                                probes.append(w)
+                    else:
+                        if w in concepts:
+                            probes.append(w)
+        if LEMMATIZE:
+            probes = set([p for p in probes if p in vocab])  # lemmas may not be in vocab
+
+        for probe in probes:  # TODO whcih features to use?
+            print(probe)
+            print(df.loc[(df['Concept'] == probe) & (df['BR_Label'] == 'encyclopaedic')])
+        raise SystemExit
+
+
+        # write to file
+        out_path = config.Dirs.tasks / 'semantic_features' / '{}_{}.txt'.format(CORPUS_NAME, vocab_size)
+        if not out_path.parent.exists():
+            out_path.parent.mkdir()
+        with out_path.open('w') as f:
+            print('Writing {}'.format(out_path))
+            for probe in probes:
+
+                # TODO
+
+                if not features:
+                    continue
+                print(features)
+                line = probe
+                for f in features:
+                    if f not in vocab or f == probe:
+                        continue
+                    line.append(' {}'.format(f))
+                f.write(line + '\n')
+                print(line)
