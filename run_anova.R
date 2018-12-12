f07a28ea8f9ece9e42ee4353716aec37f228afbf
phueb <phueb001@ucr.edu>
2018-12-11 21:53:34 -0600

    add: R-script for ANOVA

---
+++ run_anova.R
@@ -0,0 +1,17 @@
+# Title     : 2-stage NLP ANOVA
+# Objective : do significance testing for CogSci 2019 paper
+# Created by: ph
+# Created on: 12/11/18
+
+# use CTL + Numpad2 to pushc code to R console (open R Console from Tools
+
+dat = read.csv(file='matching.csv', header=TRUE, sep=",", row.names=1)
+levels(dat$group)
+
+boxplot(score~embedder,data=dat)
+
+res.aov <- aov(score ~ embedder, data = dat)
+summary(res.aov)
+TukeyHSD(res.aov)
+
+plot(res.aov, 1)
