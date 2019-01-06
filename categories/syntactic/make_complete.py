b6449134a0d6db26bd243f4ed6012e4b49b9fa4c
phueb <phueb001@ucr.edu>
2019-01-05 22:41:31 -0600

    add: extended syntactic cohyponym task with spacy

--- categories/syntactic/make_complete.py
+++ categories/syntactic/make_complete.py
@@ -2,25 +2,22 @@ import pandas as pd
 
 from src import config
 
-DIFFICULTY = 'easy'  # TODO
+CORPUS_NAME = 'childes-20180319'
+DIFFICULTY = 'easy'  # TODO  ['easy' 'medium' 'hard']
+MAX = 200  # without a maximum, the write buffer fails (i think) because file is incomplete
 
-df = pd.read_csv('spacy_pos_data_childes.txt', sep="\t",
-                 header=None)
+df = pd.read_csv('{}_pos_data.txt'.format(CORPUS_NAME), sep="\t", header=None)
 df.columns = ['word', 'pos', 'num1', 'num2', 'num3', 'difficulty']
-
 pos_list = df['pos'].unique()
 pos2words = {pos: [] for pos in pos_list}
 for pos in pos_list:
-    pos_words = df[(df['pos'] == pos) & (df['difficulty'] == DIFFICULTY)]['word'].values.tolist()
+    pos_words = df[(df['pos'] == pos) & (df['difficulty'] == DIFFICULTY)]['word'].values.tolist()[:MAX]
     pos2words[pos] = pos_words
-print(pos2words)
 
-p = config.Dirs.tasks / 'cohyponyms' / 'syntactic' / 'complete.txt'
-with p.open('w') as f:
-    for pos, pos_words in pos2words.items():
-        for probe in pos_words:
-            relata = [w for w in pos2words[pos] if w != probe]
-            relata = ' '.join(relata)
-            line = '{} {}\n'.format(probe, relata)
-            print(line.strip('\n'))
+p = config.Dirs.categories / 'syntactic' / '{}_complete.txt'.format(CORPUS_NAME)
+with open(str(p), 'w') as f:
+    for pos in pos_list:
+        for probe in pos2words[pos]:
+            line = '{} {}\n'.format(probe, pos.upper())
             f.write(line)
+            print(line)
