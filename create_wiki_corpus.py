2c26f636873e82974be9609b2f029a0ccad59c78
phueb <phueb001@ucr.edu>
2019-01-24 17:45:04 -0600

    remove wiki corpus

--- create_wiki_corpus.py
+++
@@ -1,8 +0,0 @@
-from src import wikicorpus
-import sys
-
-corpus_path = sys.argv[1]
-corpus_name = sys.argv[2]
-
-wikicorp = wikicorpus.Corpus()
-wikicorp.generate_wiki_corpus(corpus_path, corpus_name)
