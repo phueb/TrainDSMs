2d0f01856a57bdbe7813cc68c76320bb59fc5669
phueb <info@philhuebner.com>
2019-02-07 17:26:13 -0600

    add: calc num total neg + pos pairs in task

--- analyze/count_pairs.py
+++ analyze/count_pairs.py
@@ -16,14 +16,22 @@ for p in config.Dirs.tasks.rglob('{}_{}*.txt'.format(CORPUS_NAME, NUM_VOCAB)):
     pairs = set()
     pairs.update()
     num_possible = 0
+    probes = set()
     for line in lines:
         probe = line.split()[0]
         relata = line.split()[1:]
         pairs.update(list(product([probe], relata)))
+        probes.update([probe] + relata)
         num_possible += len(relata)
     print(p.relative_to(config.Dirs.tasks))
-    print('Num pairs={:,}'.format(len(pairs)))
-    print('Num possible={:,}'.format(num_possible))
+    print('Num positive pairs={:,}'.format(len(pairs)))
+    print('Num positive possible={:,}'.format(num_possible))
+
+    # TODO test
+    print('NUm probes={}'.format(len(probes)))
+    num_total = len(probes) ** 2
+
+    print('Num total possible={:,}'.format(num_total))
     diff = num_possible - len(pairs)
     if diff > 0:
         print('WARNING: Difference={}. Duplicates exist'.format(diff))
